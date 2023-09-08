75225ecf9d912d45f9a3a31b1959f17406547e72
AmirDashti <Mramir10840@gmail.com>
2023-09-08 16:29:42 +0330

    Completing the skills section on the view side

--- Resume_Project/Models/MySkillsViewModel.cs
+++ Resume_Project/Models/MySkillsViewModel.cs
@@ -10,9 +10,12 @@ public class MySkillsViewModel
     [Required(ErrorMessage = "لطفا {0} را انتخاب کنید")]
     public string Title { get; set; }
 
+    [DisplayName("سطح مبتدی ")]
     public bool Normal { get; set; }
 
+    [DisplayName("سطح متوسط ")]
     public bool Medium { get; set; }
 
+    [DisplayName("سطح پیشرفته ")]
     public bool Professional { get; set; }
 }
---
+++ Resume_Project/ViewComponents/MySkillViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume_Project.Data;
+
+namespace Resume_Project.ViewComponents
+{
+    public class MySkillViewComponent: ViewComponent
+    {
+        private MyResumeContext _context;
+
+        public MySkillViewComponent(MyResumeContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var skill = _context.Skills;
+
+            return View("MySkill", skill);
+        }
+    }
+}
